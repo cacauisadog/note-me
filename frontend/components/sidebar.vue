fb338d9b239e9cfb47098f13f924a5b4c4dd2596
cauez0rd <caue_bc@yahoo.com.br>
2018-11-05 23:28:00 -0200

    make sidebar display note title and note body preview

--- frontend/components/sidebar.vue
+++ frontend/components/sidebar.vue
@@ -5,31 +5,33 @@
       fixed
       app
     >
-      <v-list dense>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+      <v-list two-line>
+          <template v-for="(note, index) in $store.getters.getAllNotes">
+            <v-list-tile
+              :key="note.title"
+              avatar
+              ripple
+              @click="$store.commit('setCurrentNoteIndex', index)"
+            >
+              <v-list-tile-content>
+                <v-list-tile-sub-title class="text--primary">{{ note.title }}</v-list-tile-sub-title>
+                <v-list-tile-sub-title>{{ note.body }}</v-list-tile-sub-title>
+              </v-list-tile-content>
+
+            </v-list-tile>
+            <v-divider
+              v-if="index + 1 < $store.state.notes.length"
+              :key="index"
+            ></v-divider>
+          </template>
+        </v-list>
     </v-navigation-drawer>
 </template>
 
 <script>
-export default {
-    props: ['state']
-}
+ export default {
+    props: ['state'],
+  }
 </script>
 
 <style>
