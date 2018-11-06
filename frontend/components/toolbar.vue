012ab4d208c68e4e40b85fecd1793e0464a31796
cauez0rd <caue_bc@yahoo.com.br>
2018-11-06 12:00:07 -0200

    add save, add and delete icons to toolbar

--- frontend/components/toolbar.vue
+++ frontend/components/toolbar.vue
@@ -2,6 +2,20 @@
     <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="state.drawer = !state.drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Note Me</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>save_alt</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>note_add</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>delete</v-icon>
+      </v-btn>
     </v-toolbar>
 </template>
 
