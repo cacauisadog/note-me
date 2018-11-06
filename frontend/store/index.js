6433a625913005dbfe16a2d64677ac47fde2a985
cauez0rd <caue_bc@yahoo.com.br>
2018-11-05 23:27:20 -0200

    create dummy notes, getters and mutations

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -3,17 +3,53 @@ import Vuex from 'vuex';
 const createStore = () => {
     return new Vuex.Store({
         state: () => ({
-            note: {
-                title: 'Note Title',
-                body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! Laudan"
-            }
+            notes: [
+                {
+                    title: 'A Test Note',
+                    body: "AAAAALorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto"
+                },
+                {
+                    title: "B Test Note",
+                    body: "BBBBBBBLorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
+                },
+                {
+                    title: "C Test Note",
+                    body: "CCCCCCCLorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
+                },
+                {
+                    title: "D Test Note",
+                    body: "DDDDDDLorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
+                },
+                {
+                    title: "E Test Note",
+                    body: "EEEEEEEELorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
+                }
+            ],
+            currentNoteIndex: 0,
         }),
         getters: {
-            getNoteTitle: state => {
-                return state.note.title;
+            getAllNotes: state => {
+                return state.notes;
             },
-            getNoteBody: state => {
-                return state.note.body;
+            getCurrentNoteTitle: state => {
+                return state.notes[state.currentNoteIndex].title;
+            },
+            getCurrentNoteBody: state => {
+                return state.notes[state.currentNoteIndex].body;
+            }
+        },
+        mutations: {
+            setCurrentNoteIndex: (state, index) => {
+                state.currentNoteIndex = index;
+                console.log('new index: ' + index);
+            },
+            setCurrentNoteTitle: (state, title) => {
+                state.notes[state.currentNoteIndex].title = title;
+                console.log('title changed');
+            },
+            setCurrentNoteBody: (state, body) => {
+                state.notes[state.currentNoteIndex].body = body;
+                console.log('body changed');
             }
         }
     });
