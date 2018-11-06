dc687f2c83782c9f74792efea05c602278143aac
cauez0rd <caue_bc@yahoo.com.br>
2018-11-06 14:15:58 -0200

    add "add note" and "delete current note" buttons

--- frontend/components/toolbar.vue
+++ frontend/components/toolbar.vue
@@ -9,19 +9,39 @@
         <v-icon>save_alt</v-icon>
       </v-btn>
 
-      <v-btn icon>
+      <v-btn 
+        icon
+        @click="addNewNote"
+      >
         <v-icon>note_add</v-icon>
       </v-btn>
 
-      <v-btn icon>
+      <v-btn 
+        icon
+        @click="deleteCurrentNote"
+      >
         <v-icon>delete</v-icon>
       </v-btn>
     </v-toolbar>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+// TODO: usar mapMutations ao invés de declarar método por método
+
 export default {
-    props: ['state']
+    props: ['state'],
+    methods: {
+      addNewNote() {
+        this.$store.commit('addNewNote');
+        console.log("new note added via toolbar")
+      },
+      deleteCurrentNote() {
+        this.$store.commit('deleteCurrentNote');
+        console.log(`note on index ${this.$store.state.currentNoteIndex} was deleted`)
+        this.$store.commit('setCurrentNoteIndex', 0);
+      }
+    }
 }
 </script>
 
