7c325cb40bd8b85b2efc137fbe15b9e00b5ed16e
cauez0rd <caue_bc@yahoo.com.br>
2018-11-07 23:08:59 -0200

    delete default notes object and change addNewNote method

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -3,28 +3,7 @@ import Vuex from 'vuex';
 const createStore = () => {
     return new Vuex.Store({
         state: () => ({
-            notes: [
-                {
-                    title: 'A Test Note',
-                    body: "AAAAALorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto consequatur! LaudanLorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sint, nesciunt deserunt alias rem, delectus molestiae officia expedita minus id eveniet, recusandae accusantium sapiente voluptatem. Deleniti eos rerum pariatur voluptas ratione at amet quos eveniet cupiditate dolore. Vitae at beatae iusto"
-                },
-                {
-                    title: "B Test Note",
-                    body: "BBBBBBBLorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
-                },
-                {
-                    title: "C Test Note",
-                    body: "CCCCCCCLorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
-                },
-                {
-                    title: "D Test Note",
-                    body: "DDDDDDLorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
-                },
-                {
-                    title: "E Test Note",
-                    body: "EEEEEEEELorem, ipsum dolor sit amet consectetur adipisicing elit.Dicta quas quisquam illum nam ab accusamus corrupti voluptatibus dolore porro ratione, reprehenderit hic cupiditate at voluptatem? Vel incidunt assumenda natus sequi."
-                }
-            ],
+            notes: null,
             currentNoteIndex: 0,
         }),
         getters: {
@@ -51,11 +30,12 @@ const createStore = () => {
                 state.notes[state.currentNoteIndex].body = body;
                 console.log('body changed');
             },
-            addNewNote: state => {
+            addNewNote: (state, id) => {
                 state.notes.push(
                     {
-                        title: "New note",
-                        body: ""
+                        title: "New Note",
+                        body: "New body",
+                        id
                     }
                 )
             },
